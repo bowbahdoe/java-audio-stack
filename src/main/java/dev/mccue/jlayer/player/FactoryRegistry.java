6d73211f53dfc85ae2a41291ea835c3ff397fd15
Ethan McCue <ethan@mccue.dev>
2023-10-20 07:53:49 -0400

    Generify FactoryRegistry implementation

--- src/main/java/dev/mccue/jlayer/player/FactoryRegistry.java
+++ src/main/java/dev/mccue/jlayer/player/FactoryRegistry.java
@@ -50,7 +50,8 @@ public class FactoryRegistry extends AudioDeviceFactory
 	}
 
 	
-	protected Hashtable factories = new Hashtable();
+	protected final Hashtable<Class<? extends AudioDeviceFactory>, AudioDeviceFactory> factories
+			= new Hashtable<>();
 	
 	/**
 	 * Registers an <code>AudioDeviceFactory</code> instance
@@ -61,7 +62,7 @@ public class FactoryRegistry extends AudioDeviceFactory
 		factories.put(factory.getClass(), factory);						  
 	}
 	
-	public void removeFactoryType(Class cls)
+	public void removeFactoryType(Class<? extends AudioDeviceFactory> cls)
 	{
 		factories.remove(cls);
 	}
@@ -111,10 +112,10 @@ public class FactoryRegistry extends AudioDeviceFactory
 			{
 				fa = new AudioDeviceFactory[size];
 				int idx = 0;
-				Enumeration e = factories.elements();
+				Enumeration<AudioDeviceFactory> e = factories.elements();
 				while (e.hasMoreElements())
 				{
-					AudioDeviceFactory factory = (AudioDeviceFactory)e.nextElement();
+					AudioDeviceFactory factory = e.nextElement();
 					fa[idx++] = factory;	
 				}
 			}
