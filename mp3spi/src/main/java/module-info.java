bb15499dfc3c3a3d9a0fa37207a319ee8845c091
Ethan McCue <ethan@mccue.dev>
2023-12-29 00:08:04 -0500

    Bump some stuff and clean up modules

--- mp3spi/src/main/java/module-info.java
+++ mp3spi/src/main/java/module-info.java
@@ -1,12 +1,11 @@
 module dev.mccue.mp3spi {
+    requires java.desktop;
+    requires dev.mccue.jlayer.decoder;
+    requires dev.mccue.tritonus.share;
+
     exports dev.mccue.mp3spi;
     exports dev.mccue.mp3spi.mpeg.sampled.file;
     exports dev.mccue.mp3spi.mpeg.sampled.convert;
-
-    requires transitive java.desktop;
-
-    requires dev.mccue.jlayer.decoder;
-    requires dev.mccue.tritonus.share;
 
     provides javax.sound.sampled.spi.AudioFileReader with
             dev.mccue.mp3spi.mpeg.sampled.file.MpegAudioFileReader;
